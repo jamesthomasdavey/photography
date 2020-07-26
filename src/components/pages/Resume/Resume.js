2449acfe185cb61cf06482a78402952cd2dc82e8
James Thomas Davey <james.t.davey@gmail.com>
2020-07-25 20:09:03 -0700

    add the webdev section

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -22,7 +22,7 @@ const Resume = () => {
         </div>
         <section aria-labelledby='webAccessibilitySpecialist'>
           <h2 className={classes.titleHeading} id='webAccessibilitySpecialist'>
-            Web Accessibility Specialist
+            Accessibility Analyst
           </h2>
           <ul className={classes.subtitleList}>
             <li>Seattle, WA</li>
@@ -45,7 +45,7 @@ const Resume = () => {
           </h2>
           <section aria-labelledby='levelAccess'>
             <h3 className={classes.dateRoleWrapper} id='levelAccess'>
-              <div className={classes.dateHeading}>Mar, 2019 - Current</div>
+              <div className={classes.dateHeading}>Mar, 2019 - Present</div>
               <div className={classes.roleHeading}>
                 Level Access, Accessibility Analyst
               </div>
@@ -53,9 +53,9 @@ const Resume = () => {
             <div className={classes.expContent}>
               <p>
                 Responsible for conducting accessibility audits and design
-                reviews to comply with a multitude of legal standards. Provided
-                consultation services for organizations to minimize risk of
-                their websites, mobile apps, and software.
+                reviews to comply with legal standards, helping organizations to
+                minimize risk for their websites, mobile applications, and
+                software.
               </p>
               <ul>
                 <li>
@@ -64,18 +64,42 @@ const Resume = () => {
                   federal government and showcase their accessibility awareness.
                 </li>
                 <li>
-                  Coached large teams of developers and designers from Fortune
+                  Trained large teams of developers and designers from Fortune
                   500 companies of varying industries.
                 </li>
                 <li>
-                  Led remediation efforts to meet WCAG criteria by collaborating
-                  with developers, providing guidance ranging from high-level
-                  direction to code-level recommendations.
+                  Led remediation efforts to meet WCAG success criteria,
+                  providing guidance ranging from high-level direction to
+                  code-level recommendations.
                 </li>
                 <li>
                   Oversaw long-term projects and maintained strong client
                   relationships, resulting in multiple future sales and service
                   product renewals.
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section aria-labelledby='freelance'>
+            <h3 className={classes.dateRoleWrapper} id='freelance'>
+              <div className={classes.dateHeading}>Mar, 2018 - Mar, 2019</div>
+              <div className={classes.roleHeading}>
+                Freelance, Web Developer
+              </div>
+            </h3>
+            <div className={classes.expContent}>
+              <p>
+                Designed and launched web applications using modern front-end
+                technology for businesses and personal commissions.
+              </p>
+              <ul>
+                <li>
+                  Built websites using VanillaJS and JavaScript frameworks and
+                  libraries such as React, Mongoose, and Express with Node.js.
+                </li>
+                <li>
+                  Implemented advanced JavaScript concepts such as promises,
+                  async/await, callbacks, recursion, and closures.
                 </li>
               </ul>
             </div>
@@ -95,9 +119,8 @@ const Resume = () => {
               </p>
               <ul>
                 <li>
-                  Edited and organized images and videos ranging from portraits,
-                  event coverage, and product photography. Optimized digital
-                  assets for online and print delivery in large volumes.
+                  Edited, organized, and optimized digital assets for online and
+                  print delivery in large volumes.
                 </li>
                 <li>
                   Wrote documentation for software, camera, and lighting of
@@ -107,6 +130,24 @@ const Resume = () => {
               </ul>
             </div>
           </section>
+          {/* <section aria-labelledby='digitProduction'>
+            <h3 className={classes.dateRoleWrapper} id='digitProduction'>
+              <div className={classes.dateHeading}>Aug, 2013 - Aug, 2015</div>
+              <div className={classes.roleHeading}>
+                Digit Production and Post, Editor (tentative)
+              </div>
+            </h3>
+            <div className={classes.expContent}>
+              <p>
+                Responsible for producing and editing videos ranging from
+                non-profit fundraising pitches to corporate industrials.
+              </p>
+              <ul>
+                <li>...</li>
+                <li>...</li>
+              </ul>
+            </div>
+          </section> */}
         </section>
         <section
           className={classes.educationSection}
@@ -125,11 +166,15 @@ const Resume = () => {
             <div className={classes.educationContent}>
               <p>
                 <span>B.A. in Film and Technocultural Studies (2014)</span>
-                <span>Cumulative GPA: 3.72/4.00</span>
-                <span>
+                {/* <span>Cumulative GPA: 3.72/4.00</span> */}
+                {/* <span>
+                  Coursework included computer information systems,
+                  communications, and video production.
+                </span> */}
+                {/* <span>
                   Placed on the Dean's Honors List for five consecutive
                   quarters.
-                </span>
+                </span> */}
               </p>
             </div>
           </section>
