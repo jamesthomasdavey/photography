d94d88f6d7ab19689c8077a94115017dfaf65fe6
James Thomas Davey <james.t.davey@gmail.com>
2020-11-16 12:14:45 -0800

    remove cover of background image

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -19,7 +19,6 @@ class Home extends React.Component {
             <p className={classes.date}>11/16/2020</p>
           </div>
         </div>
-        <div className={classes.bgCover} />
         <ImageFadeIn
           style={{
             height: '100%',
