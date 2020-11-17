7b34bf73aeaa89bdac7c4461cd687f204b038fc7
James Thomas Davey <james.t.davey@gmail.com>
2020-11-16 17:21:43 -0800

    upload html-generated version of pdf resume

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -5,7 +5,9 @@ import classes from './Resume.module.css';
 import downloadicon from './img/downloadicon.svg';
 import resumePdf from './pdf/James Thomas Davey - Resume.pdf';
 
-const Resume = () => {
+import resumeData from './../../../data/resumeData';
+
+const ResumePrint = () => {
   document.title = 'Resume | James Thomas Davey';
   document
     .querySelector('html')
@@ -13,7 +15,7 @@ const Resume = () => {
   return (
     <div className={classes.wrapper}>
       <main className={classes.wrapper__inner}>
-        <h1 className={classes.nameHeading}>James Thomas Davey</h1>
+        <h1 className={classes.nameHeading}>{resumeData.name}</h1>
         <div className={classes.downloadLinkWrapper}>
           <a
             className={classes.downloadLink}
@@ -23,165 +25,57 @@ const Resume = () => {
             <img alt='Download Resume' src={downloadicon} />
           </a>
         </div>
-        <section aria-labelledby='webAccessibilitySpecialist'>
-          <h2 className={classes.titleHeading} id='webAccessibilitySpecialist'>
-            Accessibility Analyst
-          </h2>
+        <section>
+          <h2 className={classes.titleHeading}>{resumeData.jobTitle}</h2>
           <ul className={classes.subtitleList}>
-            <li>Seattle, WA</li>
+            <li>{resumeData.location}</li>
             <li>
-              <a href='mailto:james.davey@fastmail.com'>
-                james.davey@fastmail.com
-              </a>
+              <a href='mailto:james.davey@fastmail.com'>{resumeData.email}</a>
             </li>
-            <li>(415) 307-8703</li>
+            <li>{resumeData.phoneNumber}</li>
           </ul>
-          <p className={classes.summary}>
-            Digital accessibility expert with DHS Trusted Tester certification,
-            specializing in front-end web development, WCAG and Section 508
-            conformance, and educating developers to improve usability in the
-            digital sphere.
-          </p>
+          <p className={classes.summary}>{resumeData.summary}</p>
         </section>
-        <section aria-labelledby='workExperience'>
-          <h2 className={classes.subHeading} id='workExperience'>
-            Work Experience
-          </h2>
-          <section aria-labelledby='levelAccess'>
-            <h3 className={classes.dateRoleWrapper} id='levelAccess'>
-              <div className={classes.dateHeading}>Mar, 2019 - Present</div>
-              <div className={classes.roleHeading}>
-                Level Access, Senior Accessibility Analyst
-              </div>
-            </h3>
-            <div className={classes.expContent}>
-              <p>
-                Responsible for conducting accessibility audits and design
-                reviews to comply with legal standards, helping organizations to
-                minimize risk for their websites, mobile applications, and
-                software.
-              </p>
-              <ul>
-                <li>
-                  Authored VPAT documents following comprehensive audits within
-                  tight deadlines, enabling organizations to sell into the
-                  federal government and showcase their accessibility awareness.
-                </li>
-                <li>
-                  Trained large teams of developers and designers from Fortune
-                  500 companies of varying industries.
-                </li>
-                <li>
-                  Led remediation efforts to meet WCAG success criteria,
-                  providing guidance ranging from high-level direction to
-                  code-level recommendations.
-                </li>
-                <li>
-                  Oversaw long-term projects and maintained strong client
-                  relationships, resulting in multiple future sales and service
-                  product renewals.
-                </li>
-              </ul>
-            </div>
-          </section>
-          <section aria-labelledby='freelance'>
-            <h3 className={classes.dateRoleWrapper} id='freelance'>
-              <div className={classes.dateHeading}>Mar, 2018 - Mar, 2019</div>
-              <div className={classes.roleHeading}>
-                Freelance, Web Developer
-              </div>
-            </h3>
-            <div className={classes.expContent}>
-              <p>
-                Designed and launched web applications using modern front-end
-                technology for businesses and personal commissions.
-              </p>
-              <ul>
-                <li>
-                  Built websites using VanillaJS and JavaScript frameworks and
-                  libraries such as React, Mongoose, and Express with Node.js.
-                </li>
-                <li>
-                  Implemented advanced JavaScript concepts such as promises,
-                  async/await, callbacks, recursion, and closures.
-                </li>
-              </ul>
-            </div>
-          </section>
-          <section aria-labelledby='orangePhotography'>
-            <h3 className={classes.dateRoleWrapper} id='orangePhotography'>
-              <div className={classes.dateHeading}>Mar, 2016 - Mar, 2018</div>
-              <div className={classes.roleHeading}>
-                Orange Photography, Digital Tech
-              </div>
-            </h3>
-            <div className={classes.expContent}>
-              <p>
-                Responsible for designing photo templates, configuring image
-                software, and supporting a team of photographers, videographers,
-                and technicians.
-              </p>
-              <ul>
-                <li>
-                  Edited, organized, and optimized digital assets for online and
-                  print delivery in large volumes.
-                </li>
-                <li>
-                  Wrote documentation for software, camera, and lighting of
-                  complex studio setups, streamlining the creation of marketable
-                  photo products.
-                </li>
-              </ul>
-            </div>
-          </section>
-          {/* <section aria-labelledby='digitProduction'>
-            <h3 className={classes.dateRoleWrapper} id='digitProduction'>
-              <div className={classes.dateHeading}>Aug, 2013 - Aug, 2015</div>
-              <div className={classes.roleHeading}>
-                Digit Production and Post, Editor (tentative)
-              </div>
-            </h3>
-            <div className={classes.expContent}>
-              <p>
-                Responsible for producing and editing videos ranging from
-                non-profit fundraising pitches to corporate industrials.
-              </p>
-              <ul>
-                <li>...</li>
-                <li>...</li>
-              </ul>
-            </div>
-          </section> */}
+        <section>
+          <h2 className={classes.subHeading}>Work Experience</h2>
+          {resumeData.workExperiences.map(workExperience => {
+            return (
+              <section>
+                <h3 className={classes.dateRoleWrapper}>
+                  <div className={classes.dateHeading}>
+                    {workExperience.dates}
+                  </div>
+                  <div className={classes.roleHeading}>
+                    {workExperience.company}, {workExperience.title}
+                  </div>
+                </h3>
+                <div className={classes.expContent}>
+                  <p>{workExperience.summary}</p>
+                  <ul>
+                    {workExperience.responsibilities.map(responsibility => {
+                      return <li>{responsibility}</li>;
+                    })}
+                  </ul>
+                </div>
+              </section>
+            );
+          })}
         </section>
-        <section
-          className={classes.educationSection}
-          aria-labelledby='education'
-        >
+        <section className={classes.educationSection}>
           <h2
             className={[classes.subHeading, classes.educationHeading].join(' ')}
-            id='education'
           >
             Education
           </h2>
-          <section aria-labelledby='ucDavis'>
+          <section>
             <h3
               className={[classes.roleHeading, classes.schoolHeading].join(' ')}
-              id='ucDavis'
             >
-              University of California, Davis
+              {resumeData.education.school}
             </h3>
             <div className={classes.educationContent}>
               <p>
-                <span>B.A. in Film and Technocultural Studies (2014)</span>
-                {/* <span>Cumulative GPA: 3.72/4.00</span> */}
-                {/* <span>
-                  Coursework included computer information systems,
-                  communications, and video production.
-                </span> */}
-                {/* <span>
-                  Placed on the Dean's Honors List for five consecutive
-                  quarters.
-                </span> */}
+                <span>{resumeData.education.degree}</span>
               </p>
             </div>
           </section>
@@ -191,15 +85,9 @@ const Resume = () => {
             Technical Skills
           </h2>
           <ul className={classes.skillsList}>
-            <li>WCAG</li>
-            <li>Section 508</li>
-            <li>ADA</li>
-            <li>User Experience</li>
-            <li>Inclusive Design</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React</li>
+            {resumeData.technicalSkills.map(technicalSkill => {
+              return <li>{technicalSkill}</li>;
+            })}
           </ul>
         </section>
       </main>
@@ -207,4 +95,4 @@ const Resume = () => {
   );
 };
 
-export default Resume;
+export default ResumePrint;
---
+++ src/data/resumeData.js
@@ -0,0 +1,63 @@
+const resumeData = {
+  name: 'James Thomas Davey',
+  jobTitle: 'Accessibility Analyst',
+  location: 'Seattle, WA',
+  email: 'james.davey@fastmail.com',
+  phoneNumber: '(415)-307-8703',
+  summary:
+    'Digital accessibility expert with DHS Trusted Tester certification, specializing in front-end web development, WCAG and Section 508 conformance, and educating developers to improve usability in the digital sphere.',
+  workExperiences: [
+    {
+      dates: 'Mar, 2019 - Present',
+      company: 'Level Access',
+      title: 'Senior Accessibility Analyst',
+      summary:
+        'Responsible for conducting accessibility audits and design reviews to comply with legal standards, helping organizations to minimize risk for their websites, mobile applications, and software.',
+      responsibilities: [
+        'Authored VPAT documents following comprehensive audits within tight deadlines, enabling organizations to sell into the federal government and showcase their accessibility awareness.',
+        'Trained large teams of developers and designers from Fortune 500 companies of varying industries.',
+        'Led remediation efforts to meet WCAG success criteria, providing guidance ranging from high-level direction to code-level recommendations.',
+        'Oversaw long-term projects and maintained strong client relationships, resulting in multiple future sales and service product renewals.',
+      ],
+    },
+    {
+      dates: 'Mar, 2018 - Mar, 2019',
+      company: 'Freelance',
+      title: 'Web Developer',
+      summary:
+        'Designed and launched web applications using modern front-end technology for businesses and personal commissions.',
+      responsibilities: [
+        'Built websites using VanillaJS and JavaScript frameworks and libraries such as React, Mongoose, and Express with Node.js.',
+        'Implemented advanced JavaScript concepts such as promises, async/await, callbacks, recursion, and closures.',
+      ],
+    },
+    {
+      dates: 'Mar, 2016 - Mar, 2018',
+      company: 'Orange Photography',
+      title: 'Digital Tech',
+      summary:
+        'Responsible for designing photo templates, configuring image software, and supporting a team of photographers, videographers, and technicians.',
+      responsibilities: [
+        'Edited, organized, and optimized digital assets for online and print delivery in large volumes.',
+        'Wrote documentation for software, camera, and lighting of complex studio setups, streamlining the creation of marketable photo products.',
+      ],
+    },
+  ],
+  education: {
+    school: 'University of California, Davis',
+    degree: 'B.A. in Film and Technocultural Studies (2014)',
+  },
+  technicalSkills: [
+    'WCAG',
+    'Section 508',
+    'ADA',
+    'User Experience',
+    'Inclusive Design',
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+  ],
+};
+
+export default resumeData;
