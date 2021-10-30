f77e04dca15e0f0952f67553985fc5120ec53fae
James Thomas Davey <jamesthomasdavey@james-air.lan>
2021-10-29 17:08:54 -0700

    mend build failure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // packages
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
-} from 'react-router-dom';
+} from "react-router-dom";
 
 // layout components
 // import Navigation from './compone/nts/layout/Navigation/Navigation';
@@ -13,26 +13,23 @@ import {
 
 // page components
 // import Contact from './components/pages/Contact/Contact';
-import Home from './components/pages/Home/Home';
-import Resume from './components/pages/Resume/Resume';
-// import ResumePrint from './components/pages/ResumePrint/ResumePrint';
+import Home from "./components/pages/Home/Home";
 
 class App extends React.Component {
   state = {};
   render() {
-    document.title = 'James Thomas Davey';
+    document.title = "James Thomas Davey";
     return (
       <Router>
         {/* <Navigation /> */}
         {/* <Main> */}
         <Switch>
           {/* <Route exact path='/contact' component={Contact} /> */}
-          <Route exact path='/' component={Home} />
-          <Route exact path='/home'>
-            <Redirect to='/' />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home">
+            <Redirect to="/" />
           </Route>
-          <Route exact path='/resume' component={Resume} />
-          <Route path='/*' component={Home} />
+          <Route path="/*" component={Home} />
           {/* <Route exact path='/resume-print' component={ResumePrint} /> */}
         </Switch>
         {/* </Main> */}
