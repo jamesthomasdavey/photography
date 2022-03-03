1043807c7defc2544977865f67276ff09eb867a9
James Davey <james.davey@fastmail.com>
2022-03-03 10:46:03 -0800

    use exif data for alt text

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,185 +1,151 @@
 // packages
-import React from 'react';
+import React from "react";
 
 // components
-import Image from './components/Image/Image';
-import ContactForm from './components/ContactForm/ContactForm';
+import Image from "./components/Image/Image";
+import ContactForm from "./components/ContactForm/ContactForm";
 
 // css
-import classes from './Home.module.css';
+import classes from "./Home.module.css";
 
 // img
-import img0001 from './img/0001.jpg';
-import img0002 from './img/0002.jpg';
-import img0003 from './img/0003.jpg';
-import img0004 from './img/0004.jpg';
-import img0005 from './img/0005.jpg';
-import img0006 from './img/0006.jpg';
-import img0007 from './img/0007.jpg';
-import img0008 from './img/0008.jpg';
-import img0009 from './img/0009.jpg';
-import img0010 from './img/0010.jpg';
-import img0011 from './img/0011.jpg';
-import img0012 from './img/0012.jpg';
-import img0013 from './img/0013.jpg';
-import img0014 from './img/0014.jpg';
-import img0015 from './img/0015.jpg';
-import img0016 from './img/0016.jpg';
-import img0017 from './img/0017.jpg';
-import img0018 from './img/0018.jpg';
-import img0019 from './img/0019.jpg';
-import img0020 from './img/0020.jpg';
-import img0021 from './img/0021.jpg';
-import img0022 from './img/0022.jpg';
-import img0023 from './img/0023.jpg';
-import img0024 from './img/0024.jpg';
-import img0025 from './img/0025.jpg';
-import img0026 from './img/0026.jpg';
-import img0028 from './img/0027.jpg';
-import img0027 from './img/0028.jpg';
-import img0029 from './img/0029.jpg';
-import img0030 from './img/0030.jpg';
-import img0031 from './img/0031.jpg';
-import img0032 from './img/0032.jpg';
-import img0033 from './img/0033.jpg';
-import img0034 from './img/0034.jpg';
+import img0001 from "./img/0001.jpg";
+import img0002 from "./img/0002.jpg";
+import img0003 from "./img/0003.jpg";
+import img0004 from "./img/0004.jpg";
+import img0005 from "./img/0005.jpg";
+import img0006 from "./img/0006.jpg";
+import img0007 from "./img/0007.jpg";
+import img0008 from "./img/0008.jpg";
+import img0009 from "./img/0009.jpg";
+import img0010 from "./img/0010.jpg";
+import img0011 from "./img/0011.jpg";
+import img0012 from "./img/0012.jpg";
+import img0013 from "./img/0013.jpg";
+import img0014 from "./img/0014.jpg";
+import img0015 from "./img/0015.jpg";
+import img0016 from "./img/0016.jpg";
+import img0017 from "./img/0017.jpg";
+import img0018 from "./img/0018.jpg";
+import img0019 from "./img/0019.jpg";
+import img0020 from "./img/0020.jpg";
+import img0021 from "./img/0021.jpg";
+import img0022 from "./img/0022.jpg";
+import img0023 from "./img/0023.jpg";
+import img0024 from "./img/0024.jpg";
+import img0025 from "./img/0025.jpg";
+import img0026 from "./img/0026.jpg";
+import img0028 from "./img/0027.jpg";
+import img0027 from "./img/0028.jpg";
+import img0029 from "./img/0029.jpg";
+import img0030 from "./img/0030.jpg";
+import img0031 from "./img/0031.jpg";
+import img0032 from "./img/0032.jpg";
+import img0033 from "./img/0033.jpg";
+import img0034 from "./img/0034.jpg";
 
 const images = [
   {
     src: img0001,
-    alt: '',
   },
   {
     src: img0002,
-    alt: '',
   },
   {
     src: img0003,
-    alt: '',
   },
   {
     src: img0004,
-    alt: '',
   },
   {
     src: img0005,
-    alt: '',
   },
   {
     src: img0006,
-    alt: '',
   },
   {
     src: img0007,
-    alt: '',
   },
   {
     src: img0008,
-    alt: '',
   },
   {
     src: img0009,
-    alt: '',
   },
   {
     src: img0010,
-    alt: '',
   },
   {
     src: img0011,
-    alt: '',
   },
   {
     src: img0012,
-    alt: '',
   },
   {
     src: img0013,
-    alt: '',
   },
   {
     src: img0014,
-    alt: '',
   },
   {
     src: img0015,
-    alt: '',
   },
   {
     src: img0016,
-    alt: '',
   },
   {
     src: img0017,
-    alt: '',
   },
   {
     src: img0018,
-    alt: '',
   },
   {
     src: img0019,
-    alt: '',
   },
   {
     src: img0020,
-    alt: '',
   },
   {
     src: img0021,
-    alt: '',
   },
   {
     src: img0022,
-    alt: '',
   },
   {
     src: img0023,
-    alt: '',
   },
   {
     src: img0024,
-    alt: '',
   },
   {
     src: img0025,
-    alt: '',
   },
   {
     src: img0026,
-    alt: '',
   },
   {
     src: img0027,
-    alt: '',
   },
   {
     src: img0028,
-    alt: '',
   },
   {
     src: img0029,
-    alt: '',
   },
   {
     src: img0030,
-    alt: '',
   },
   {
     src: img0031,
-    alt: '',
   },
   {
     src: img0032,
-    alt: '',
   },
   {
     src: img0033,
-    alt: '',
   },
   {
     src: img0034,
-    alt: '',
   },
 ];
 
--- src/components/pages/Home/components/Image/Image.js
+++ src/components/pages/Home/components/Image/Image.js
@@ -1,37 +1,59 @@
 // packages
-import React, { useState } from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import React, { useState } from "react";
+import exifr from "exifr";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 // css
-import classes from './Image.module.css';
+import classes from "./Image.module.css";
 
-const Image = ({ source, alt }) => {
+const Image = ({ source }) => {
   const [imageLoaded, setImageLoaded] = useState(false);
+  const [imageMetadata, setImageMetadata] = useState({
+    loaded: false,
+    alt: "",
+    landscape: false,
+  });
+
   const handleImageLoad = () => {
     setImageLoaded(true);
   };
   const showImage = () => {
-    if (imageLoaded) {
+    if (imageLoaded && imageMetadata.loaded) {
       return (
         <div
           className={classes.img}
           style={{
             backgroundImage: `url(${source})`,
           }}
-          aria-label={alt ? alt : undefined}
+          aria-hidden={imageMetadata.alt ? undefined : "true"}
+          aria-label={imageMetadata.alt}
         />
       );
     }
   };
+
+  if (!imageMetadata.loaded) {
+    exifr
+      .parse(source)
+      .then((metadata) => {
+        setImageMetadata({
+          loaded: true,
+          alt: metadata.ImageDescription ? metadata.ImageDescription : "",
+          landscape: metadata.ExifImageHeight < metadata.ExifImageWidth,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
   const hideLoader = () => {
     if (!imageLoaded) {
       return (
         <LazyLoadImage
           src={source}
-          height='0'
-          width='0'
+          height="0"
+          width="0"
           afterLoad={handleImageLoad}
-          alt=''
+          alt=""
         />
       );
     }
