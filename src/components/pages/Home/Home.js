d9436e6ebf9fd9a45e6ebd887d3f83cac5e8c626
James Davey <james.davey@fastmail.com>
2023-07-24 18:58:41 -0700

    add better-compressed images with alt text

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,47 +1,51 @@
 // packages
-import React from 'react';
+import React from "react";
 
 // components
-import Image from './components/Image/Image';
+import Image from "./components/Image/Image";
 
 // css
-import classes from './Home.module.css';
+import classes from "./Home.module.css";
 
 // img
-import img0001 from './img/0001.jpg';
-import img0002 from './img/0002.jpg';
-import img0003 from './img/0003.jpg';
-import img0004 from './img/0004.jpg';
-import img0005 from './img/0005.jpg';
-import img0006 from './img/0006.jpg';
-import img0007 from './img/0007.jpg';
-import img0008 from './img/0008.jpg';
-import img0009 from './img/0009.jpg';
-import img0010 from './img/0010.jpg';
-import img0011 from './img/0011.jpg';
-import img0012 from './img/0012.jpg';
-import img0013 from './img/0013.jpg';
-import img0014 from './img/0014.jpg';
-import img0015 from './img/0015.jpg';
-import img0016 from './img/0016.jpg';
-import img0017 from './img/0017.jpg';
-import img0018 from './img/0018.jpg';
-import img0019 from './img/0019.jpg';
-import img0020 from './img/0020.jpg';
-import img0021 from './img/0021.jpg';
-import img0022 from './img/0022.jpg';
-import img0023 from './img/0023.jpg';
-import img0024 from './img/0024.jpg';
-import img0025 from './img/0025.jpg';
-import img0026 from './img/0026.jpg';
-import img0028 from './img/0027.jpg';
-import img0027 from './img/0028.jpg';
-import img0029 from './img/0029.jpg';
-import img0030 from './img/0030.jpg';
-import img0031 from './img/0031.jpg';
-import img0032 from './img/0032.jpg';
-import img0033 from './img/0033.jpg';
-import img0034 from './img/0034.jpg';
+import img0001 from "./img/0001.jpg";
+import img0002 from "./img/0002.jpg";
+import img0003 from "./img/0003.jpg";
+import img0004 from "./img/0004.jpg";
+import img0005 from "./img/0005.jpg";
+import img0006 from "./img/0006.jpg";
+import img0007 from "./img/0007.jpg";
+import img0008 from "./img/0008.jpg";
+import img0009 from "./img/0009.jpg";
+import img0010 from "./img/0010.jpg";
+import img0011 from "./img/0011.jpg";
+import img0012 from "./img/0012.jpg";
+import img0013 from "./img/0013.jpg";
+import img0014 from "./img/0014.jpg";
+import img0015 from "./img/0015.jpg";
+import img0016 from "./img/0016.jpg";
+import img0017 from "./img/0017.jpg";
+import img0018 from "./img/0018.jpg";
+import img0019 from "./img/0019.jpg";
+import img0020 from "./img/0020.jpg";
+import img0021 from "./img/0021.jpg";
+import img0022 from "./img/0022.jpg";
+import img0023 from "./img/0023.jpg";
+import img0024 from "./img/0024.jpg";
+import img0025 from "./img/0025.jpg";
+import img0026 from "./img/0026.jpg";
+import img0028 from "./img/0027.jpg";
+import img0027 from "./img/0028.jpg";
+import img0029 from "./img/0029.jpg";
+import img0030 from "./img/0030.jpg";
+import img0031 from "./img/0031.jpg";
+import img0032 from "./img/0032.jpg";
+import img0033 from "./img/0033.jpg";
+import img0034 from "./img/0034.jpg";
+import img0035 from "./img/0035.jpg";
+import img0036 from "./img/0036.jpg";
+import img0037 from "./img/0037.jpg";
+import img0038 from "./img/0038.jpg";
 
 const images = [
   {
@@ -145,6 +149,18 @@ const images = [
   },
   {
     src: img0034,
+  },
+  {
+    src: img0035,
+  },
+  {
+    src: img0036,
+  },
+  {
+    src: img0037,
+  },
+  {
+    src: img0038,
   },
 ];
 
